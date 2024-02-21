64f3f8882ad4191e0cd5bbc2395a715887accded
Aziz Muntasir <160031061+muntasir24@users.noreply.github.com>
2024-02-21 18:45:45 +0600

    Update Fight_with_Bits.cpp

--- Fight_with_Bits.cpp
+++ Fight_with_Bits.cpp
@@ -3,3 +3,21 @@ bool isPowerOfTwo(int n) {
       return n > 0 && (n & n - 1) == 0;
     }
 _____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+2.
+long long previous_power_of_two(long long n) {
+    int position = 0;
+
+    // Finding the position of the highest set bit
+    while (n > 1) {
+        n = n >> 1;  // Right shifting the number by 1
+        position++; // Incrementing the position
+    }
+
+    // Returning the previous power of two
+    return (1LL << position);
+}
+________________________________________________________
+.For n = 14, the binary representation is 1110.
+.The highest set bit is in position 3 (from the right).
+.Therefore, the previous power of two is 2^3 = 8.
+___________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
