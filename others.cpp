51cb9bfb2a081f3e65d3835b950cd4f3732eba1b
Aziz Muntasir <160031061+muntasir24@users.noreply.github.com>
2024-02-24 16:27:59 +0600

    Create others.cpp

---
+++ others.cpp
@@ -0,0 +1,12 @@
+//vector pair
+        vector<pair<int,int>> vp(n);
+for (int i = 0; i < n; ++i)
+        {
+          cin>>vp[i].second;
+        
+        }
+        for (int i = 0; i < n; ++i)
+        {
+          cin>>x;
+          vp[i].first=abs(x);
+        }
