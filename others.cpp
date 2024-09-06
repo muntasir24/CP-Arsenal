850dc56ba8b2a0f732826c27e331dfc31878d689
Aziz Muntasir <160031061+muntasir24@users.noreply.github.com>
2024-09-06 12:06:46 +0600

    Update others.cpp

--- others.cpp
+++ others.cpp
@@ -10,3 +10,10 @@ for (int i = 0; i < n; ++i)
           cin>>x;
           vp[i].first=abs(x);
         }
+
+_________________________________________________________________
+      cout << "The number with 3 decimal places is: ";
+
+  cout << fixed << setprecision(3) << num;
+output : 1.000
+________________________________________________________________________
