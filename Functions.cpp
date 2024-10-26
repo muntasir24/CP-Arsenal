d72e0324e0db2b005a3896a8df601945dd0dd60f
Aziz Muntasir <160031061+muntasir24@users.noreply.github.com>
2024-10-26 13:09:04 +0600

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -8,4 +8,16 @@ bool isPrime(int n) {
             return false;
     return true;
 }
+______________________________________________________________________
+#find all the divisiors of a number in root n :
 
+vector<ll> divisors;
+    for(ll i=2;i*i <= n;i++){
+        if(n%i == 0){
+            divisors.push_back(i);
+            if(n/i != i){
+                divisors.push_back(n/i);
+            }
+        }
+    }
+_____________________________________________________________________
